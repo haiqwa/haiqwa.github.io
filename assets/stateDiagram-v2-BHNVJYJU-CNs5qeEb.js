33b76ef1661778bd48ad368522906212f8d4360f
王海权 <1926627357@qq.com>
2026-06-10 15:53:43 +0800

    feat: 添加数学公式渲染 (KaTeX)

---
+++ assets/stateDiagram-v2-BHNVJYJU-CNs5qeEb.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-AQP2D5EJ-fGq57ErT.js";import{_ as i}from"./index-DpbFbGoZ.js";import"./chunk-55IACEB6-DMGhm4Fm.js";import"./chunk-2J33WTMH-CEh1LqMA.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
--- assets/stateDiagram-v2-BHNVJYJU-DeGeFd6r.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-AQP2D5EJ-BIY5X3kF.js";import{_ as i}from"./index-5gCdo5-Q.js";import"./chunk-55IACEB6-lr32Wbbl.js";import"./chunk-2J33WTMH-Cq3Sx0Oh.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
